a18e34e07cf4c79f89c43a2e26e44298b967701b
Marc Schulmeister <marcschulm@gmail.com>
2022-04-18 10:40:32 -0700

    database now concurrent with front end state

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # swiperz-api
+
+## Commands
+
+### `. venv/bin/activate`
+starts flask virtual environment in the swiper-api root dir
+
+### `python3 app.py`
+starts swiperz-api app in the swiper-api/venv dir on localhost:5000
--- venv/init_db.py
+++ venv/init_db.py
@@ -15,24 +15,31 @@ cur.execute('DROP TABLE IF EXISTS games;')
 cur.execute('CREATE TABLE games (id serial PRIMARY KEY,'
                                  'player1 varchar (150) NOT NULL,'
                                  'player2 varchar (150) NOT NULL,'
+                                 'current_player varchar (150) NOT NULL,'
                                  'game_over varchar (150) NOT NULL DEFAULT FALSE,'
+                                 'message varchar (150),'
+                                 'board int[][],'
                                  'date_added date DEFAULT CURRENT_TIMESTAMP);'
                                  )
 
 # Insert data into the table
 
-cur.execute('INSERT INTO games (player1, player2, game_over)'
-            'VALUES (%s, %s, %s)',
+cur.execute('INSERT INTO games (player1, player2, current_player, game_over, board)'
+            'VALUES (%s, %s, %s, %s, %s)',
             ('marc',
              'wenyan',
-             'true')
+             '1',
+             'true',
+             '{{0, 1, 1} , {1, 1, 1}, {0, 0, 0}}')
             )
 
-cur.execute('INSERT INTO games (player1, player2, game_over)'
-            'VALUES (%s, %s, %s)',
+cur.execute('INSERT INTO games (player1, player2, current_player, game_over, board)'
+            'VALUES (%s, %s, %s, %s, %s)',
             ('bob',
              'bill',
-             'false')
+             '2',
+             'false',
+             '{{0, 0, 0} , {0, 0, 0}, {0, 0, 0}}')
             )
 
 conn.commit()
