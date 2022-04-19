546ae8892eb4c90336efb0c15e618ea986b28b90
Marc Schulmeister <marcschulm@gmail.com>
2022-04-18 20:51:43 -0700

    install CORS, add JSON

--- venv/app.py
+++ venv/app.py
@@ -1,7 +1,9 @@
 import psycopg2
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',
@@ -19,6 +21,16 @@ def index():
     cur.close()
     conn.close()
     return render_template('index.html', games=games)
+
+@app.route('/json')
+def json():
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM games;')
+    games = cur.fetchall()
+    cur.close()
+    conn.close()
+    return jsonify(games)
 
 if __name__ == "__main__":
   app.run(debug=True)
