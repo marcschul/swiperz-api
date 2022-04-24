710b926187261e6cf25041d75ef3f4c66b047ada
Marc Schulmeister <marcschulm@gmail.com>
2022-04-24 10:53:50 -0700

    multiplayer feature complete

--- venv/app.py
+++ venv/app.py
@@ -50,17 +50,21 @@ def games():
   if request.method == 'GET':
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute('SELECT * FROM games;')
-    games = cur.fetchall()
+    # cur.execute('SELECT * FROM games;')
+    # games = cur.fetchall()
+    cur.execute('SELECT * FROM games WHERE id=(SELECT MAX(id) FROM games);')
+    lastGame = cur.fetchall()
     cur.close()
     conn.close()
-    return jsonify(games[len(games) - 1])
+    return jsonify(lastGame[0])
 
   if request.method == 'POST':
     data = request.json
     print(data)
     conn = get_db_connection()
     cur = conn.cursor()
+    # cur.execute("SELECT * FROM games;")
+    # games = cur.fetchall()
     cur.execute('INSERT INTO games (player1, player2, current_player, game_over, message, board)'
             'VALUES (%s, %s, %s, %s, %s, %s)',
             ('Player1',
@@ -71,11 +75,12 @@ def games():
              '{{0, 0, 0, 0, 0, 0, 0} , {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0}}')
             )
     conn.commit()
-    cur.execute("SELECT * FROM games;")
-    games = cur.fetchall()
+    cur.execute('SELECT * FROM games WHERE id=(SELECT MAX(id) FROM games);')
+    lastGame = cur.fetchall()
+    print(lastGame)
     cur.close()
     conn.close()
-    return jsonify(games[len(games) - 1])
+    return jsonify(lastGame[0])
 
   if request.method == 'PUT':
     data = request.json
@@ -92,9 +97,13 @@ def games():
     print(len(games))
     cur.execute('UPDATE games SET current_player=(%s), game_over=(%s), message=(%s), board=(%s) WHERE id=(%s);', [currentPlayer, gameOver, message, board, len(games)])
     conn.commit()
+    cur.execute('SELECT * FROM games WHERE id=(SELECT MAX(id) FROM games);')
+    lastGame = cur.fetchall()
+    print(games[len(games) - 1])
+    print(lastGame[0])
     cur.close()
     conn.close()
-    return jsonify(games[len(games) - 1])
+    return jsonify(lastGame[0])
 
 if __name__ == "__main__":
   app.run(debug=True)
